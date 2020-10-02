02d16dca957fd0259f37e1d54dbe7742e65aaff4
Albina <zagrevskaia.as@students.dvfu.ru>
2020-10-02 22:16:05 +1000

    Fix3

--- main.cpp
+++ main.cpp
@@ -3,5 +3,8 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "My name is Albina." << std::endl;
+    int sum=0;
+    for (int i=0;i<10;i++) sum+=i;
+    std::cout << "sum=" << sum << std::endl;
     return 0;
 }
