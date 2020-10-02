a3fdb9f1878fd6818056baf3f2b2be4d8ac41773
Albina <zagrevskaia.as@students.dvfu.ru>
2020-10-02 22:26:51 +1000

    Fix5

--- main.cpp
+++ main.cpp
@@ -7,5 +7,12 @@ int main() {
     for (int i=0;i<10;i++) sum+=i;
     std::cout << "sum=" << sum << std::endl;
     std::cout << "I'm 17 years old." << std::endl;
+    int i=0;
+    int num=1;
+    while (i<5) {
+        num+=2;
+        i++;
+    }
+    std::cout << "I have an " << num << "-years-old sister" << std::endl;
     return 0;
 }
